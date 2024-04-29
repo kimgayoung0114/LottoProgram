77d9c76348b18163b17c5b4619d039781cffc27b
pjjoo <pjjoo@192.168.219.164>
2024-04-30 00:01:35 +0900

    구매페이지 - 초기화, 수동, 자동, 반자동 가능 - 삭제 가능

--- lottoTeamProject( with주현이언니 )/src/Purchase.java
+++ lottoTeamProject( with주현이언니 )/src/Purchase.java
@@ -1,6 +1,9 @@
+
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 
 import java.awt.Color;
 import java.awt.Component;
@@ -14,40 +17,32 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
-public class PurchasePage extends JFrame implements ActionListener {
-	private Map<String, ArrayList<Integer>> listSelectedNumber;
+public class Purchase extends JFrame implements ActionListener {
 	private JToggleButton[] numberButtons;
-//   private Map<JPanel, JButton[]> panelButtonMap;
+	private Map<JPanel, JButton[]> panelButtonMap;
 	private ArrayList<JPanel> panelList;
 	private ArrayList<Integer> selectedNumbers;
 	private JPanel currentPanel;
 	private JPanel previousPanel;
 
-	public PurchasePage() {
+	public Purchase() {
 		setTitle("동행복권");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(900, 600);
+		setSize(900, 560);
 
 		getContentPane().setBackground(Color.WHITE);
 		getContentPane().setLayout(null);
 
 		selectedNumbers = new ArrayList<>();
-		// 맵 초기화
-        listSelectedNumber = new TreeMap<>();
-        
 
 		JPanel pnlButtons = new JPanel();
-		pnlButtons.setBounds(21, 107, 285, 337);
+		pnlButtons.setBounds(21, 105, 285, 296);
 		getContentPane().add(pnlButtons);
 		numberButtons = new JToggleButton[45];
 		for (int i = 0; i < 45; i++) {
@@ -60,7 +55,7 @@ public class PurchasePage extends JFrame implements ActionListener {
 
 		JButton btnReset = new JButton("초기화");
 		btnReset.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnReset.setBounds(22, 459, 82, 37);
+		btnReset.setBounds(22, 421, 82, 37);
 		getContentPane().add(btnReset);
 		btnReset.addActionListener(new ActionListener() {
 			@Override
@@ -73,14 +68,14 @@ public class PurchasePage extends JFrame implements ActionListener {
 				}
 				// 선택된 숫자들을 저장하는 리스트 초기화
 				selectedNumbers.clear();
-				// 패널 업데이트를 통해 기존에 표시된 숫자들을 제거하고, UI를 초기 상태로 복구
-				// updatePanels();
+
 			}
 
 		});
+
 		JButton btnAuto = new JButton("자동");
 		btnAuto.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnAuto.setBounds(126, 459, 82, 37);
+		btnAuto.setBounds(123, 421, 82, 37);
 		getContentPane().add(btnAuto);
 		btnAuto.addActionListener(new ActionListener() {
 			@Override
@@ -109,57 +104,20 @@ public class PurchasePage extends JFrame implements ActionListener {
 
 		JButton btnOk = new JButton("확인");
 		btnOk.addActionListener(new ActionListener() {
-			private int currentPanelIndex;
+			private int currentPanelIndex = 0;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// 사용자가 선택한 숫자가 6개가 아닌 경우 경고 다이얼로그 표시
-				if (selectedNumbers.size() != 6) {
-					JOptionPane.showMessageDialog(null, "숫자 6개를 선택해주세요.", "경고", JOptionPane.WARNING_MESSAGE);
-					return; // 추가 작업을 진행하지 않고 리턴
-				}
-
-				// 이전 패널의 수정 및 삭제 버튼 비활성화
-				// enablePanelButtons(currentPanel);
-
 				// 현재 패널에 선택된 번호 표시
-				Collections.sort(selectedNumbers); // 오름차순으로 정렬
 				updatePanels();
-				System.out.println("현재 선택한 숫자들: " + selectedNumbers);
 
-				// 현재 패널에 선택된 번호 저장
-                String panelKey = "";
-                switch(currentPanelIndex) {
-                    case 0:
-                        panelKey = "A";
-                        break;
-                    case 1:
-                        panelKey = "B";
-                        break;
-                    case 2:
-                        panelKey = "C";
-                        break;
-                    case 3:
-                        panelKey = "D";
-                        break;
-                    case 4:
-                        panelKey = "E";
-                        break;
-                }
-                listSelectedNumber.put(panelKey, new ArrayList<>(selectedNumbers));
+				enablePanelButtons(currentPanel);
 
-                // 선택된 번호 초기화
-                selectedNumbers.clear();
-
-//				System.out.println("A에저장된 값:" + listSelectedNumber.get("A"));
-//				System.out.println("B에저장된 값:" + listSelectedNumber.get("B"));
-//				System.out.println("A에저장된 값:" + listSelectedNumber.get("A"));
-//
-//				System.out.println(selectedNumbers);
+				// 선택된 번호 초기화
+				selectedNumbers.clear();
 
 				// 다음 패널 선택
 				if (currentPanelIndex < panelList.size() - 1) {
-					// enablePanelButtons(currentPanel);
 
 					currentPanelIndex++;
 					currentPanel = panelList.get(currentPanelIndex);
@@ -177,7 +135,6 @@ public class PurchasePage extends JFrame implements ActionListener {
 					btnOk.setEnabled(false);
 				}
 				resetButtons();
-
 			}
 
 			private void resetButtons() {
@@ -189,101 +146,109 @@ public class PurchasePage extends JFrame implements ActionListener {
 				}
 			}
 
-			// 기존에 주석 처리된 메소드들은 필요에 따라 구현
-
 		});
 
 		btnOk.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnOk.setBounds(225, 459, 82, 37);
+		btnOk.setBounds(225, 420, 82, 37);
 		getContentPane().add(btnOk);
 
 		JPanel panelA = new JPanel();
-		panelA.setBounds(392, 134, 285, 43);
+		panelA.setBounds(389, 105, 285, 43);
 		getContentPane().add(panelA);
 
 		JPanel panelB = new JPanel();
-		panelB.setBounds(392, 197, 285, 43);
+		panelB.setBounds(389, 165, 285, 43);
 		getContentPane().add(panelB);
 
 		JPanel panelC = new JPanel();
-		panelC.setBounds(392, 264, 285, 43);
+		panelC.setBounds(389, 232, 285, 43);
 		getContentPane().add(panelC);
 
 		JPanel panelD = new JPanel();
-		panelD.setBounds(392, 328, 285, 43);
+		panelD.setBounds(389, 296, 285, 43);
 		getContentPane().add(panelD);
 
 		JPanel panelE = new JPanel();
-		panelE.setBounds(394, 390, 285, 43);
+		panelE.setBounds(391, 358, 285, 43);
 		getContentPane().add(panelE);
 
 		JButton btnUpdateA = new JButton("수정");
-		btnUpdateA.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		btnUpdateA.setEnabled(true);
+		btnUpdateA.setEnabled(false);
 		btnUpdateA.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnUpdateA.setBounds(688, 134, 72, 43);
+		btnUpdateA.setBounds(685, 105, 72, 43);
 		getContentPane().add(btnUpdateA);
 
 		JButton btnUpdateB = new JButton("수정");
-		btnUpdateB.setEnabled(true);
+		btnUpdateB.setEnabled(false);
 		btnUpdateB.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnUpdateB.setBounds(688, 197, 72, 43);
+		btnUpdateB.setBounds(685, 165, 72, 43);
 		getContentPane().add(btnUpdateB);
 
 		JButton btnUpdateC = new JButton("수정");
-		btnUpdateC.setEnabled(true);
+		btnUpdateC.setEnabled(false);
 		btnUpdateC.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnUpdateC.setBounds(688, 264, 72, 43);
+		btnUpdateC.setBounds(685, 232, 72, 43);
 		getContentPane().add(btnUpdateC);
 
 		JButton btnUpdateD = new JButton("수정");
-		btnUpdateD.setEnabled(true);
+		btnUpdateD.setEnabled(false);
 		btnUpdateD.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnUpdateD.setBounds(688, 328, 72, 43);
+		btnUpdateD.setBounds(685, 296, 72, 43);
 		getContentPane().add(btnUpdateD);
 
 		JButton btnUpdateE = new JButton("수정");
-		btnUpdateE.setEnabled(true);
+		btnUpdateE.setEnabled(false);
 		btnUpdateE.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnUpdateE.setBounds(690, 390, 72, 43);
+		btnUpdateE.setBounds(687, 358, 72, 43);
 		getContentPane().add(btnUpdateE);
 
 		JButton btnDeleteA = new JButton("삭제");
-		btnDeleteA.setEnabled(true);
+		btnDeleteA.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// 해당 패널의 원들을 모두 제거
+		        panelA.removeAll();
+		        // 패널을 다시 그리도록 강제 호출
+		        panelA.revalidate();
+		        panelA.repaint();
+		        
+		        // 사용자가 선택한 숫자들을 초기화
+		        selectedNumbers.clear();
+			}
+			
+		});
+		btnDeleteA.setEnabled(false);
 		btnDeleteA.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnDeleteA.setBounds(779, 134, 72, 43);
+		btnDeleteA.setBounds(776, 105, 72, 43);
 		getContentPane().add(btnDeleteA);
 
 		JButton btnDeleteB = new JButton("삭제");
-		btnDeleteB.setEnabled(true);
+		btnDeleteB.setEnabled(false);
 		btnDeleteB.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnDeleteB.setBounds(780, 197, 69, 43);
+		btnDeleteB.setBounds(777, 165, 69, 43);
 		getContentPane().add(btnDeleteB);
 
 		JButton btnDeleteC = new JButton("삭제");
-		btnDeleteC.setEnabled(true);
+		btnDeleteC.setEnabled(false);
 		btnDeleteC.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		btnDeleteC.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnDeleteC.setBounds(779, 264, 72, 43);
+		btnDeleteC.setBounds(776, 232, 72, 43);
 		getContentPane().add(btnDeleteC);
 
 		JButton btnDeleteD = new JButton("삭제");
-		btnDeleteD.setEnabled(true);
+		btnDeleteD.setEnabled(false);
 		btnDeleteD.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnDeleteD.setBounds(779, 328, 72, 43);
+		btnDeleteD.setBounds(776, 296, 72, 43);
 		getContentPane().add(btnDeleteD);
 
 		JButton btnDeleteE = new JButton("삭제");
-		btnDeleteE.setEnabled(true);
+		btnDeleteE.setEnabled(false);
 		btnDeleteE.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnDeleteE.setBounds(781, 390, 72, 43);
+		btnDeleteE.setBounds(778, 358, 72, 43);
 		getContentPane().add(btnDeleteE);
 		panelList = new ArrayList<>();
 		panelList.add(panelA);
@@ -291,12 +256,12 @@ public class PurchasePage extends JFrame implements ActionListener {
 		panelList.add(panelC);
 		panelList.add(panelD);
 		panelList.add(panelE);
-//      panelButtonMap = new HashMap<>();
-//      panelButtonMap.put(panelA, new JButton[] { btnUpdateA, btnDeleteA });
-//      panelButtonMap.put(panelB, new JButton[] { btnUpdateB, btnDeleteB });
-//      panelButtonMap.put(panelB, new JButton[] { btnUpdateC, btnDeleteC });
-//      panelButtonMap.put(panelB, new JButton[] { btnUpdateD, btnDeleteD });
-//      panelButtonMap.put(panelB, new JButton[] { btnUpdateE, btnDeleteE });
+		panelButtonMap = new HashMap<>();
+		panelButtonMap.put(panelA, new JButton[] { btnUpdateA, btnDeleteA });
+		panelButtonMap.put(panelB, new JButton[] { btnUpdateB, btnDeleteB });
+		panelButtonMap.put(panelC, new JButton[] { btnUpdateC, btnDeleteC });
+		panelButtonMap.put(panelD, new JButton[] { btnUpdateD, btnDeleteD });
+		panelButtonMap.put(panelE, new JButton[] { btnUpdateE, btnDeleteE });
 
 		// Initially set current panel and enable buttons for it
 		currentPanel = panelA;
@@ -304,23 +269,47 @@ public class PurchasePage extends JFrame implements ActionListener {
 
 		JLabel lblNewLabel = new JLabel("구매 금액:");
 		lblNewLabel.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		lblNewLabel.setBounds(395, 456, 65, 29);
+		lblNewLabel.setBounds(431, 430, 65, 29);
 		getContentPane().add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(470, 456, 99, 29);
+		lblNewLabel_1.setBounds(506, 430, 99, 29);
 		getContentPane().add(lblNewLabel_1);
 
 		JButton btnNewButton_1 = new JButton("구매하기");
 		btnNewButton_1.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnNewButton_1.setBounds(633, 452, 146, 37);
+		btnNewButton_1.setBounds(669, 426, 146, 37);
 		getContentPane().add(btnNewButton_1);
 
 		JLabel lblNewLabel_2 = new JLabel("");
-		// lblNewLabel_2.setIcon(new
-		// ImageIcon(PurchasePage.class.getResource("/image/동행복권.png")));
+		lblNewLabel_2.setIcon(new ImageIcon(Purchase.class.getResource("/image/동행복권.png")));
 		lblNewLabel_2.setBounds(279, 10, 211, 90);
 		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("A");
+		lblNewLabel_3.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
+		lblNewLabel_3.setBounds(360, 111, 26, 29);
+		getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblB = new JLabel("B");
+		lblB.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
+		lblB.setBounds(360, 168, 26, 29);
+		getContentPane().add(lblB);
+		
+		JLabel lblC = new JLabel("C");
+		lblC.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
+		lblC.setBounds(360, 235, 26, 29);
+		getContentPane().add(lblC);
+		
+		JLabel lblD = new JLabel("D");
+		lblD.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
+		lblD.setBounds(360, 299, 26, 29);
+		getContentPane().add(lblD);
+		
+		JLabel lblE = new JLabel("E");
+		lblE.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 17));
+		lblE.setBounds(360, 361, 26, 29);
+		getContentPane().add(lblE);
 
 		setVisible(true);
 	}
@@ -354,6 +343,17 @@ public class PurchasePage extends JFrame implements ActionListener {
 				button.setSelected(false);
 			}
 		}
+	}
+
+// 수정 삭제 버튼 활성화
+	private void enablePanelButtons(JPanel currentPanel) {
+		JButton[] buttons = panelButtonMap.get(currentPanel);
+		if (buttons != null) {
+			for (JButton button : buttons) {
+				button.setEnabled(true);
+			}
+		}
+
 	}
 
 	private void updatePanels() {
@@ -426,6 +426,11 @@ public class PurchasePage extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		new PurchasePage();
+		// Event Dispatch Thread (EDT)에서 GUI를 생성하고 보여주기
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Purchase(); // 애플리케이션의 메인 윈도우 생성
+			}
+		});
 	}
 }
